351262e79f6ec2296ffa575d0a2c8a75ba6cf4cf
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-06-06 14:36:38 +0700

    update komponen/coursel.js

---
+++ komponen/coursel.js
@@ -0,0 +1,26 @@
+const sliderItems = document.querySelectorAll('.slider-item');
+    let currentItemIndex = 0;
+
+    function showSlide(index) {
+      sliderItems.forEach((item, i) => {
+        if (i === index) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
+
+    function nextSlide() {
+      currentItemIndex = (currentItemIndex + 1) % sliderItems.length;
+      showSlide(currentItemIndex);
+    }
+
+    function prevSlide() {
+      currentItemIndex = (currentItemIndex - 1 + sliderItems.length) % sliderItems.length;
+      showSlide(currentItemIndex);
+    }
+
+    showSlide(currentItemIndex);
+    setInterval(nextSlide, 3000);
+  
